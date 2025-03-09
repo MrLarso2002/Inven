0925fbebeaca628ed709c4e1cb94e6e010e1ab91
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-09 18:48:37 +0100

    Update variables.js

--- design/variables.js
+++ design/variables.js
@@ -1,9 +1,6 @@
-
-
 // type name = value
 
 const { set } = require("express/lib/application");
-
 
 // local variables (default)
 local variable;
@@ -22,75 +19,5 @@ func () {};
 local func () {};
 scope func () {};
 
-// all types
-
-// any, doesnt need to be defined as variables are this by default
-:any 
-
-//strings
-:str 
-
-// single character! why? idk honestly
-:char 
-
-// arrays
-:arr 
-
-// maps
-:map 
-
-// booleans
-:bool 
-
-// js like objects
-:obj 
-
-// numberz
-:num 
-
-//functions
-:func
-
-// nothing/invalid/doesnt exist
-:null
-
-
-// things we could add:
-
-:set 
-
-:bytes 
-
-:bytearray 
-
-:memoryview 
-
-:int 
-
-:float 
-
-:complex 
-
-:dictionary //afarik this is like map and obj
-
-:double 
-
-:range 
-
-:date 
-
-:Int8Array
-
-:Int16Array
-
-:Int32Array
-
-:Int64Array //this depends on floating point stuff that idk yet
-
-:Uint8Array
-
-:Uint16Array
-
-:Uint32Array
-
-:Uint64Array
+// overwrite a function when a condition matches
+func () when () {};
