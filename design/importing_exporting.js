e0ec729d36ee28c13a41aaa7a173d461df3a8284
LarsIsHere <lars-private@hotmail.com>
2025-03-09 16:00:23 +0100

    design things

---
+++ design/importing_exporting.js
@@ -0,0 +1,13 @@
+// Design is made in js files for minimal highlighting
+
+
+// importing
+unpack @module // native modules
+unpack module // modules from files
+unpack box:module // modules from "box" package manager?
+
+
+
+// exporting
+
+package module;
---
+++ design/variables.js
@@ -0,0 +1,96 @@
+
+
+// type name = value
+
+const { set } = require("express/lib/application");
+
+
+// local variables (default)
+local variable;
+lo variable;
+
+// scoped variables
+scope variable;
+sc variable;
+
+// you can only export local variables
+package variable;
+
+
+// functions are by default local, you dont have to declare it but you can if you want to.
+func () {};
+local func () {};
+scope func () {};
+
+// all types
+
+// any, doesnt need to be defined as variables are this by default
+:any 
+
+//strings
+:str 
+
+// single character! why? idk honestly
+:char 
+
+// arrays
+:arr 
+
+// maps
+:map 
+
+// booleans
+:bool 
+
+// js like objects
+:obj 
+
+// numberz
+:num 
+
+//functions
+:func
+
+// nothing/invalid/doesnt exist
+:null
+
+
+// things we could add:
+
+:set 
+
+:bytes 
+
+:bytearray 
+
+:memoryview 
+
+:int 
+
+:float 
+
+:complex 
+
+:dictionary //afarik this is like map and obj
+
+:double 
+
+:range 
+
+:date 
+
+:Int8Array
+
+:Int16Array
+
+:Int32Array
+
+:Int64Array //this depends on floating point stuff that idk yet
+
+:Uint8Array
+
+:Uint16Array
+
+:Uint32Array
+
+:Uint64Array
