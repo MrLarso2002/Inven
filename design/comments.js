fe350d54f4aa44d3910eba24c5c640f3c6f8f018
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-09 18:56:41 +0100

    Create comments.js

---
+++ design/comments.js
@@ -0,0 +1,9 @@
+Inline comments:
+// your comment
+they start at // until the end of the line
+
+Multiline comments:
+/*
+Comments
+*/
+the area between the /* and */ can be any size and have new lines, all will be counted as a comment
