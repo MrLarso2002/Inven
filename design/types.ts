4280b2a0c282cdf50cdf356f6179b428c04b0736
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-09 19:03:21 +0100

    Update types.ts

--- design/types.ts
+++ design/types.ts
@@ -1,32 +1,32 @@
-Generics
-:any
-:err
-:null (Missing a value)
+// Generics
+:any // Anything
+:err // An error
+:null // Missing a value
 
-Letters
-:char (any utf8 character, can be multi character letters)
-:str
+// Letters
+:char // Any utf8 character, can be multi character letters, like emoji
+:str // A string
 
-Booleans
-:bool (true, false)
+// Booleans
+:bool // true or false
 
-Numbers
-:num or :number (a number, for quick implementation)
+// Numbers
+:num / :number // A number, for quick implementation
 :int{byte count}
 :uint{byte count}
-:float{32, 64}  (32 single precission, 64 double precission; could also be f{})
-byte count can be: 8, 16, 32, 64, 128
+:float / :f {32, 64} // 32 single precission, 64 double precission
+//byte count can be: 8, 16, 32, 64, 128
 
-Others
-:func
-:buffer
-:date
+// Others
+:func // A function
+:obj // A dictionary / object
+:date // A date in time
 
-Arrays
-:[type] (for example :[str] )
-For sets $[] where $ means unique
+// Arrays
+:[type] // for example :[str]
+//for sets $[] where $ means unique
 
-Types in Functions
+// Types inside Functions
 func name(argument:argumentType):returnType when(){}
-argumentType is the type the arguments need to be
-returnType is what it returns
+//argumentType is the type the arguments need to be
+//returnType is what it returns
