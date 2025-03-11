ef3f994f3cec91fd53e5436d011f0340d694649d
LarsIsHere <lars-private@hotmail.com>
2025-03-11 20:06:55 +0100

    Update frontend.rs

--- src/frontend.rs
+++ src/frontend.rs
@@ -18,9 +18,10 @@ peg::parser!{
       / expected!("identifier")
     rule module_identifier() -> String
       = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_' | '@']['a'..='z' | 'A'..='Z' | '0'..='9' | '_' ]** ".") {n.to_owned()}}
-      / expected!("module_identifier")
+      / expected!("module identifier")
     rule module_sub_identifier() -> String
       = quiet!{ n:$("{" _ [ "*" | [module_child:identifier | module_child:identifier _ "as" alt_name:identifier ]** "," ] _ "}" )}
+      / expected!("sub_module identifier")
 
     rule _() = quiet!{[' ' | '\t']*}
   }
