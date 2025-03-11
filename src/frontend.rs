4008b199a92300a2631f23f74294bdfd489bcd7b
LarsIsHere <lars-private@hotmail.com>
2025-03-11 20:05:05 +0100

    Update frontend.rs

--- src/frontend.rs
+++ src/frontend.rs
@@ -11,24 +11,17 @@ peg::parser!{
 // our parser ig for now
 peg::parser!{
   grammar inven_parser() for str {
-    pub rule import() -> () = "unpack" _ module:module_identifier _ ";" { (module) }
+    pub rule import() -> () = "unpack" _ module:module_identifier _ modulechild:module_sub_identifier? _ ";" { (module, modulechild) }
 
-/*
-unpack module.thing.thang.theng.funcA
-unpack module.thing.thang.theng.funcB
-npack module.thing.thang.theng.funcC
-
-unpack module.thing.thang.theng. {func, func as e, func}
-*/
-// fuck my mom wants me to study.... but ill try to write here as often as can- if u want dot, at least make it like this
-// lmao we both studying. its alr 
-// i can't be very active, have to study also...
     rule identifier() -> String
       = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) {n.to_owned()}}
       / expected!("identifier")
     rule module_identifier() -> String
-      = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_' | '@']['a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '.']*['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) {n.to_owned()}}
+      = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_' | '@']['a'..='z' | 'A'..='Z' | '0'..='9' | '_' ]** ".") {n.to_owned()}}
       / expected!("module_identifier")
+    rule module_sub_identifier() -> String
+      = quiet!{ n:$("{" _ [ "*" | [module_child:identifier | module_child:identifier _ "as" alt_name:identifier ]** "," ] _ "}" )}
+
     rule _() = quiet!{[' ' | '\t']*}
   }
 }
