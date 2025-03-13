6a3822fe8be494b22381cb482f65f910277d77a6
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-13 21:51:37 +0100

    Fixes

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,27 +1,24 @@
-peg::parser!{
-  grammar list_parser() for str {
-    rule number() -> u32
-      = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
-
-    pub rule list() -> Vec<u32>
-      = "[" l:(number() ** ",") "]" { l }
-  }
-}
-
 // our parser ig for now
 peg::parser!{
   grammar inven_parser() for str {
-    pub rule import() -> () = "unpack" _ module:module_identifier _ modulechild:module_sub_identifier? _ ";" { (module, modulechild) }
+    pub rule import() -> (String, Option<String>)
+      = "unpack" _ module:module_identifier() _ modulechild:module_sub_identifier()? _ ";" { (module, modulechild) }
 
     rule identifier() -> String
       = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) {n.to_owned()}}
       / expected!("identifier")
+
     rule module_identifier() -> String
       = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_' | '@']['a'..='z' | 'A'..='Z' | '0'..='9' | '_' ]** ".") {n.to_owned()}}
       / expected!("module identifier")
-    rule module_sub_identifier() -> String
-      = quiet!{ n:$("{" _ [ "*" | [module_child:identifier | module_child:identifier _ "as" alt_name:identifier ]** "," ] _ "}" )}
-      / expected!("sub_module identifier")
+
+    rule module_sub_identifier() -> (String)
+      = precedence!{
+        n:$("{" _ $( (identifier() (_ "as" identifier())? ) ** "," ) _ "}" ) {n.to_owned()}
+        --
+        n:$("{" _ "*" _ "}") {n.to_owned()}
+      }
+      / expected!("sub module identifier")
 
     rule _() = quiet!{[' ' | '\t']*}
   }
@@ -30,13 +27,17 @@ peg::parser!{
 pub fn main() {
 // e
 }
-/* 
+
+// Tests
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
   fn unpack() {
-
+    // TODO: This should pass but currently it doesn't
+    let res = inven_parser::import("unpack module;").expect("Should be Ok");
+    assert_eq!(res.0, "module");
+    assert_eq!(res.1, None);
   }
-}*/
+}
