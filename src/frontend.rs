8e81920575f60a23401fbeb2b1e872790c87583e
LarsIsHere <lars-private@hotmail.com>
2025-03-14 21:22:35 +0100

    Update frontend.rs

--- src/frontend.rs
+++ src/frontend.rs
@@ -6,6 +6,15 @@ peg::parser!{
       pub rule importmin() -> String
         = "unpack" _ module:module_identifier() _ ";" { module }
 
+    // TODO: finish variable declaration
+    pub rule declaration() -> (String, bool)
+      = variable:variable_type() __ mutable:("mut")? variable:identifier() _ (":" _ "type")? "="? _ ";" { (variable, mutable.is_some()) }
+
+
+    rule variable_type() -> String
+      = quiet!{ n:$((("local" / "lo") / ("scope" / "sc"))) {n.to_owned()}} 
+      / expected!("variable")
+
     rule identifier() -> String
       = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) {n.to_owned()}}
       / expected!("identifier")
@@ -40,10 +49,16 @@ mod tests {
   fn unpack() {
     
     // TODO: Make Multiline possible. EOF issue
-    let res = inven_parser::import("unpack module;
-    unpack module;").expect("Should be Ok");
+    let res = inven_parser::import("unpack module;").expect("Should be Ok");
     assert_eq!(res.0, "module");
     assert_eq!(res.1, None);
     assert_eq!(res.2, false); // box:
+  }
+
+  #[test]
+  fn var_decl() {
+    let res = inven_parser::declaration("local test =;").expect("Should be Ok");
+    assert_eq!(res.0, "test");
+    assert_eq!(res.1, false);
   }
 }
